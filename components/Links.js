c8e9732bf7551f23981c2a7502fe4138690f0a56
akira-wd-1224 <jda.7seven.a24@gmail.com>
2022-08-16 00:28:52 +0900

    ♻️ Linksのリファクタリング

--- components/Links.js
+++ components/Links.js
@@ -1,33 +1,38 @@
 import classes from "./Links.module.css";
 
+const ITEMS = [
+  {
+    href: "https://nextjs.org/docs",
+    title: "Documentation \u2192",
+    description: "Find in-depth information about Next.js features and API.",
+  },
+  {
+    href: "https://nextjs.org/learn",
+    title: "Learn \u2192",
+    description: "Learn about Next.js in an interactive course with quizzes!",
+  },
+  {
+    href: "https://github.com/vercel/next.js/tree/canary/examples",
+    title: "Examples \u2192",
+    description: "Discover and deploy boilerplate example Next.js projects.",
+  },
+  {
+    href: "https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
+    title: "Deploy \u2192",
+    description:
+      "Instantly deploy your Next.js site to a public URL with Vercel.",
+  },
+];
+
 export function Links() {
   return (
     <div className={classes.grid}>
-      <a href="https://nextjs.org/docs" className={classes.card}>
-        <h2 className={classes.title}>Documentation &rarr;</h2>
-        <p className={classes.description}>Find in-depth information about Next.js features and API.</p>
-      </a>
-
-      <a href="https://nextjs.org/learn" className={classes.card}>
-        <h2 className={classes.title}>Learn &rarr;</h2>
-        <p className={classes.description}>Learn about Next.js in an interactive course with quizzes!</p>
-      </a>
-
-      <a
-        href="https://github.com/vercel/next.js/tree/canary/examples"
-        className={classes.card}
-      >
-        <h2 className={classes.title}>Examples &rarr;</h2>
-        <p className={classes.description}>Discover and deploy boilerplate example Next.js projects.</p>
-      </a>
-
-      <a
-        href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        className={classes.card}
-      >
-        <h2 className={classes.title}>Deploy &rarr;</h2>
-        <p className={classes.description}>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-      </a>
+      {ITEMS.map((ITEMS) => (
+        <a key={ITEMS.href} href={ITEMS.href} className={classes.card}>
+          <h2 className={classes.title}>{ITEMS.title}</h2>
+          <p className={classes.description}>{ITEMS.description}</p>
+        </a>
+      ))}
     </div>
   );
 }
