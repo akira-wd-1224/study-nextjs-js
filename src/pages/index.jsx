c2310ec4a8ee2a2dd4e996b27ae9be6f437c9ebe
akira-wd-1224 <jda.7seven.a24@gmail.com>
2022-08-23 18:41:25 +0900

    ✨ 講義用useEffectの導入

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,7 +2,7 @@ import Head from "next/head";
 import { Footer } from "src/components/Footer";
 import { Header } from "src/components/Header";
 import { Main } from "src/components/Main";
-import { useCallback } from "react";
+import { useCallback, useEffect } from "react";
 import styles from "src/styles/Home.module.css";
 
 // const handleClick = (e) => {
@@ -19,6 +19,15 @@ export default function Home() {
     e.preventDefault();
     alert(foo);
   }, []);
+
+  useEffect(() => {
+    console.log("マウント時")
+    document.body.style.backgroundColor = "lightblue"
+    return () => {
+      console.log("アンマウント時")
+      document.body.style.backgroundColor = ""
+    }
+  }, [])
 
   return (
     <div className={styles.container}>
