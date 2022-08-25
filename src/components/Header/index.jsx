7c6f9507b0b721b50c2e42544811e98abda1454d
akira-wd-1224 <jda.7seven.a24@gmail.com>
2022-08-25 09:34:43 +0900

    ✏️ 大文字へ修正

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,10 +5,10 @@ export function Header() {
   return (
     <header className={classes.header}>
       <Link href="/">
-        <a className={classes.anchor}>index</a>
+        <a className={classes.anchor}>Index</a>
       </Link>
       <Link href="/about">
-        <a className={classes.anchor}>about</a>
+        <a className={classes.anchor}>About</a>
       </Link>
     </header>
   );
