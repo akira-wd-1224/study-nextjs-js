10700106cfc5c71d078843522c6732fce487a5c4
akira-wd-1224 <jda.7seven.a24@gmail.com>
2022-08-29 22:59:02 +0900

    ✨ SGとfallbackについて学んだ

--- src/pages/comments/[id]/index.jsx
+++ src/pages/comments/[id]/index.jsx
@@ -1,23 +1,32 @@
+import { useRouter } from "next/router";
 import { CommentComponent } from "src/components/Comment";
 import { Header } from "src/components/Header";
 import { SWRConfig } from "swr";
 
 export const getStaticPaths = async () => {
-  const comments = await fetch("https://jsonplaceholder.typicode.com/comments");
+  const comments = await fetch(
+    "https://jsonplaceholder.typicode.com/comments?_limit=10"
+  );
   const commentsData = await comments.json();
   const paths = commentsData.map((comment) => ({
     params: { id: comment.id.toString() },
   }));
 
-  return { paths, fallback: false };
+  return { paths, fallback: "blocking" };
 };
 
 export const getStaticProps = async (ctx) => {
   const { id } = ctx.params;
   const COMMENT_API_URL = `https://jsonplaceholder.typicode.com/comments/${id}`;
   const comment = await fetch(COMMENT_API_URL);
-  const commentData = await comment.json();
 
+  if (!comment.ok) {
+    return {
+      notFound: true,
+    };
+  }
+  const commentData = await comment.json();
+  console.log(`comment/${id}がSG化されました`);
   return {
     props: {
       fallback: {
