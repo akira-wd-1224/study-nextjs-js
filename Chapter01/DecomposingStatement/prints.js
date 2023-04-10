a8baa7438f1ecdec5ee80fd691dc6a320d75afaa
BG <84509898+bright-good@users.noreply.github.com>
2023-04-10 22:31:26 +0000

    関数名の変更、変数のインライン化

--- Chapter01/DecomposingStatement/prints.js
+++ Chapter01/DecomposingStatement/prints.js
@@ -47,7 +47,7 @@ function totalvolumeCredits() {
   return volumeCredits
 }
 
-function appleSauce(){
+function totalAmount(){
   let totalAmount = 0
   for (let perf of invoice.performances) {
     totalAmount += amountFor(perf);
@@ -56,13 +56,12 @@ function appleSauce(){
 }
 
 function statement(invoice, plays) {
-  let totalAmount = 0;
   let result = `Statement for ${invoice.customer}\n`;
   for (let perf of invoice.performances) {
-  result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
+    result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
   }
   
-  result += `Amount owed is ${usd(totalAmount)}\n`;
+  result += `Amount owed is ${usd(totalAmount())}\n`;
   result += `You earned ${totalvolumeCredits()} credits\n`;
   return result;
 }
