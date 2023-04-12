40ce5e0890765df06c2c5ed688fcbd027dc7165d
BG <84509898+bright-good@users.noreply.github.com>
2023-04-12 22:51:51 +0000

    playForの置き換え

--- Chapter01/DecomposingStatement/prints.js
+++ Chapter01/DecomposingStatement/prints.js
@@ -61,7 +61,7 @@ function renderPlainText(data, plays) {
 
   function amountFor(aPerformance) {
     let result = 0;
-    switch (playFor(aPerformance).type) {
+    switch (aPerformance.play.type) {
       case "tragedy":
         result = 40000;
         if (aPerformance.audience > 30) {
@@ -76,7 +76,7 @@ function renderPlainText(data, plays) {
         result += 300 * aPerformance.audience;
         break;
       default:
-        throw new Error(`unknown type: ${playFor(aPerformance).type}`);
+        throw new Error(`unknown type: ${aPerformance.play.type}`);
     }
     return result;
   }
