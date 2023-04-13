94a51f9d619f4ffa65f3e359d9663ae42c143bc8
BG <84509898+bright-good@users.noreply.github.com>
2023-04-13 22:36:34 +0000

    パイプラインによるループの置換え

--- Chapter01/DecomposingStatement/prints.js
+++ Chapter01/DecomposingStatement/prints.js
@@ -18,19 +18,13 @@ function enrichPerformance(aPerformance) {
 }
 
 function totalAmount(data) {
-  let result = 0
-  for (let perf of data.performances) {
-    result += pref.amount;
-  }
-  return result;
+  return data.performances
+    .reduce((total, p) => total + p.amount, 0);
 }
 
 function totalvolumeCredits(data) {
-  let result = 0;
-  for (let perf of data.performances) {
-    result += pref.volumeCredits;
-  }
-  return result
+  return data.preformance
+    .reduce((total, p) => total + p.volumeCredits, 0);
 }
 
 function volumeCreditsFor(aPerformance) {
