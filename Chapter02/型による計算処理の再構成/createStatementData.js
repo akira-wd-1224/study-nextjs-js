6db1943e8c60b859fc3dbba3dd3c1175dfff9b01
BG <84509898+bright-good@users.noreply.github.com>
2023-04-15 09:53:49 +0000

    関数のインライン化

--- Chapter02/型による計算処理の再構成/createStatementData.js
+++ Chapter02/型による計算処理の再構成/createStatementData.js
@@ -39,7 +39,7 @@ function enrichPerformance(aPerformance) {
     const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
     const result = Object.assign({}, aPerformance);
     result.play = calculator.play;
-    result.amount = amountFor(result);
+    result.amount = calculator.amount;
     result.volumeCredits = volumeCreditsFor(result);
     return result;
 }
