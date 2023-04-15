c2938ae6c2c0ee2da0020e2da8493fe57ad5e5f1
BG <84509898+bright-good@users.noreply.github.com>
2023-04-15 10:41:36 +0000

    ポリモフィズムによる条件記述の置き換え

--- Chapter02/型による計算処理の再構成/createStatementData.js
+++ Chapter02/型による計算処理の再構成/createStatementData.js
@@ -6,58 +6,6 @@ export default function createStatementData(invoice, plays) {
     result.totalvolumeCredits = totalvolumeCredits(result);
     return result;
 }
-class PerformanceCalculator {
-    constructor(aPerformance, aPlay) {
-        this.performance = aPerformance;
-        this.play = aPlay;
-    }
-
-    get amount() {
-        let result = 0;
-        switch (this.play.type) {
-            case "tragedy":
-                result = 40000;
-                if (this.performance.audience > 30) {
-                    result += 1000 * (this.performance.audience - 30);
-                }
-                break;
-            case "comedy":
-                result = 30000;
-                if (this.performance.audience > 20) {
-                    result += 10000 + 500 * (this.performance.audience - 20);
-                }
-                result += 300 * this.performance.audience;
-                break;
-            default:
-                throw new Error(`unknown type: ${this.play.type}`);
-        }
-        return result;
-    }
-
-    get volumeCredits() {
-        let result = 0;
-        result += Math.max(this.performance.audience - 30, 0);
-        if ("comedy" === this.play.type) result += Math.floor(this.performance.audience / 5);
-        return result;
-    }
-}
-
-function createPerformanceCalculator(aPerformance, aPlay) {
-    switch (aPlay.type) {
-        case "tragedy": return new TragendyCalculator(aPerformance, aPlay);
-        case "comedy": return new ComedyCalculator(aPerformance, aPlay);
-        default:
-            throw new Error(`unknown type: ${aPlay.type}`);
-    }
-}
-
-class TragendyCalculator extends PerformanceCalculator {
-
-}
-
-class ComedyCalculator extends PerformanceCalculator {
-
-}
 
 function enrichPerformance(aPerformance) {
     const calculator = PerformanceCalculator(aPerformance, playFor(aPerformance));
@@ -72,14 +20,6 @@ function playFor(aPerformance) {
     return plays[aPerformance.playID];
 }
 
-function amountFor(aPerformance) {
-    return new PerformanceCalculator(aPerformance, playFor(aPerformance)).amount;
-}
-
-function volumeCreditsFor(aPerformance) {
-    return new PerformanceCalculator(aPerformance, playFor(aPerformance)).volumeCredits;
-}
-
 function totalAmount(data) {
     return data.performances
         .reduce((total, p) => total + p.amount, 0);
@@ -90,4 +30,50 @@ function totalvolumeCredits(data) {
         .reduce((total, p) => total + p.volumeCredits, 0);
 }
 
+function createPerformanceCalculator(aPerformance, aPlay) {
+    switch (aPlay.type) {
+        case "tragedy": return new TragendyCalculator(aPerformance, aPlay);
+        case "comedy": return new ComedyCalculator(aPerformance, aPlay);
+        default:
+            throw new Error(`unknown type: ${aPlay.type}`);
+    }
+}
 
+class PerformanceCalculator {
+    constructor(aPerformance, aPlay) {
+        this.performance = aPerformance;
+        this.play = aPlay;
+    }
+
+    get amount() {
+        throw new Error(`サブクラスの責務`);
+    }
+
+    get volumeCredits() {
+        return Math.max(this.performance.audience - 30, 0);
+    }
+}
+
+class TragendyCalculator extends PerformanceCalculator {
+    get amount() {
+        result = 40000;
+        if (this.performance.audience > 30) {
+            result += 1000 * (this.performance.audience - 30);
+        }
+        return result;
+    }
+}
+
+class ComedyCalculator extends PerformanceCalculator {
+    get amount() {
+        result = 30000;
+        if (this.performance.audience > 20) {
+            result += 10000 + 500 * (this.performance.audience - 20);
+        }
+        result += 300 * this.performance.audience;
+    }
+
+    get volumeCredits() {
+        return super.volumeCredits + Math.floor(this.performance.audience / 5);
+    }
+}
